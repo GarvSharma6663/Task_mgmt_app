facf716af353d3b9a4242a8462f5714914f2ec48
Garv Sharma <garvsharma663@gmail.com>
2025-06-24 22:07:02 +0530

    React Initialized

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const el = document.querySelector('#root');
+const root = ReactDOM.createRoot(el);
+
+function App() {
+  return <h1>TaskDone! The Task Manager App</h1>;
+}
+
+root.render(<App />);
